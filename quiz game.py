14c72afdc1a24d5c89c8c58ddbba7e829433b27c
asma-herby <103481619+asma-herby@users.noreply.github.com>
2023-12-13 20:02:17 +0300

    Update quiz game.py

--- quiz game.py
+++ quiz game.py
@@ -1,5 +1,3 @@
-Python 3.10.4 (v3.10.4:9d38120e33, Mar 23 2022, 17:29:05) [Clang 13.0.0 (clang-1300.0.29.30)] on darwin
-Type "help", "copyright", "credits" or "license()" for more information.
 print("Wellcome to quiz game !!")
 print('NOTE: if your spelling is incorrect then it is considered as wrong answer')
 score = 0
